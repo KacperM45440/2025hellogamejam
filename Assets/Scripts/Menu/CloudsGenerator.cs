302a13725e0d49747c3f68de816301ba36ac133d
Kamila Spychała <104089237+Kamila18520@users.noreply.github.com>
2025-07-05 20:04:58 +0200

    menu v3

--- Assets/Scripts/Menu/CloudsGenerator.cs
+++ Assets/Scripts/Menu/CloudsGenerator.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloudsGenerator : MonoBehaviour
 {
+    [SerializeField] private Sprite[] clouds;
     [SerializeField] RectTransform Menu_RT;
+    private Image image;
     private float m_MenuHeight;
     private float m_MenuWidth;
 
@@ -10,6 +13,7 @@ public class CloudsGenerator : MonoBehaviour
     {
         m_MenuHeight = Menu_RT.sizeDelta.y;
         m_MenuWidth = Menu_RT.sizeDelta.x;
+        image = GetComponent<Image>();
     }
 
  
