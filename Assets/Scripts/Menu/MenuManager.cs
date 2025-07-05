8f66a7e71f12708a74c8c7b2ee5b5f899044b5f3
Kamila Spychała <104089237+Kamila18520@users.noreply.github.com>
2025-07-05 15:31:44 +0200

    Menu v1

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPanel;
+
+    public void PlayButton()
+    {
+
+
+    }
+
+    public void ExitButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // Zamyka grê po zbudowaniu
+#endif
+    }
+}
