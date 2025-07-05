aebaf291ed6cee2fb0629de049021c01470add20
Kacper Markiewicz <kacpermarkiewicz@protonmail.com>
2025-07-05 22:13:48 +0200

    Dialogue wip

--- Assets/Scripts/Clients/DialogueController.cs
+++ Assets/Scripts/Clients/DialogueController.cs
@@ -1,8 +1,80 @@
 using System.Collections.Generic;
 using UnityEngine;
+using GameStage = StageManager.GameStage;
 
 public class DialogueController : MonoBehaviour
 {
+    public StageManager StageManagerRef;
+    public DialogueData DialogueDataRef;
+    public ClientScript ClientScriptRef;
+
+    public Dialogue mainDialogue;
+    public List<string> currentDialogue;
+    public int currentSubdialogue = 0;
+    
+    [HideInInspector] public List<List<string>> DialogueGreetings;
+    [HideInInspector] public List<List<string>> DialogueRequests;
+    [HideInInspector] public List<List<string>> DialogueResponsesGood;
+    [HideInInspector] public List<List<string>> DialogueResponsesAverage;
+    [HideInInspector] public List<List<string>> DialogueResponsesBad;
+
+    public void InitializeDialogue()
+    {
+        DialogueGreetings = DialogueDataRef.CreateDialogueGreeting();
+        DialogueRequests = DialogueDataRef.CreateDialogueRequest();
+        DialogueResponsesGood = DialogueDataRef.CreateResponseGood();
+        DialogueResponsesAverage = DialogueDataRef.CreateResponseAverage();
+        DialogueResponsesBad = DialogueDataRef.CreateResponseBad();
+    }
+
+
+    public void ProgressDialogue()
+    {
+        string dialogueLine = currentDialogue[currentSubdialogue];
+        if (dialogueLine == null)
+        {
+            ProgressStage();
+        }
+    }
+
+    public void WeaponFeedback(string feedback) // string is a placeholder, should be enum
+    {
+        StageManagerRef.SetCurrentGameStage(GameStage.Response);
+
+        switch (feedback)
+        {
+            case "Good":
+                currentDialogue = mainDialogue.ResponseGood;
+                break;
+            case "Average":
+                currentDialogue = mainDialogue.ResponseAverage; 
+                break;
+            case "Bad":
+                currentDialogue = mainDialogue.ResponseBad;
+                break;
+        }
+    }
+
+    public void ProgressStage()
+    {
+        GameStage stage = StageManagerRef.GetCurrentGameStage();
+        List<string> nextDialogue = new();
+
+        switch (stage) 
+        {
+            case GameStage.EnterStore:
+                StageManagerRef.SetCurrentGameStage(GameStage.Greeting);
+                currentDialogue = mainDialogue.Greeting;
+                break;
+            case GameStage.Greeting:
+                StageManagerRef.SetCurrentGameStage(GameStage.Request);
+                currentDialogue = mainDialogue.Request;
+                break;
+            case GameStage.Response:
+                StageManagerRef.SetCurrentGameStage(GameStage.LeaveStore);
+                break;
+        }
+    }
 
     public class Dialogue
     {
--- Assets/Scripts/Clients/DialogueData.cs
+++ Assets/Scripts/Clients/DialogueData.cs
@@ -27,4 +27,48 @@ public class DialogueData : MonoBehaviour
 
         return clientGreets;
     }
+
+    public List<List<string>> CreateDialogueRequest()
+    {
+        List<List<string>> clientRequests = new List<List<string>>
+        {
+            Client1Request,
+            Client2Request,
+        };
+
+        return clientRequests;
+    }
+
+    public List<List<string>> CreateResponseGood()
+    {
+        List<List<string>> clientResponsesGood = new List<List<string>>
+        {
+            Client1ResponseGood,
+            Client2ResponseGood,
+        };
+
+        return clientResponsesGood;
+    }
+
+    public List<List<string>> CreateResponseAverage()
+    {
+        List<List<string>> clientResponsesAverage = new List<List<string>>
+        {
+            Client1ResponseAverage,
+            Client2ResponseAverage,
+        };
+
+        return clientResponsesAverage;
+    }
+
+    public List<List<string>> CreateResponseBad()
+    {
+        List<List<string>> clientResponsesBad = new List<List<string>>
+        {
+            Client1ResponseBad,
+            Client2ResponseBad,
+        };
+
+        return clientResponsesBad;
+    }
 }
